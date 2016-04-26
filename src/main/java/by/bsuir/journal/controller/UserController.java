ea76b737250ddff2b61ce8060bbc05675dc6aa1e
ViolaWittrockiana <chigir.vera@mail.ru>
2016-04-26 20:23:55 +0300

    new branch for json version

--- src/main/java/by/bsuir/journal/controller/UserController.java
+++ src/main/java/by/bsuir/journal/controller/UserController.java
@@ -9,12 +9,10 @@ import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Locale;
 
 
 
@@ -47,51 +45,6 @@ public class UserController {
 		List<User> users = userService.findAllUsers();
 		model.addAttribute("users", users);
 		return "userslist";
-	}
-
-
-	/**
-	 * This method will provide the medium to add a new user.
-	 */
-	@RequestMapping(value = {"/user-create"}, method = RequestMethod.GET)
-	public String newUser(ModelMap model) {
-		User user = new User();
-		model.addAttribute("user", user);
-		model.addAttribute("edit", false);
-		return "registration";
-	}
-
-	/**
-	 * This method will be called on form submission, handling POST request for
-	 * saving user in database. It also validates the user input
-	 */
-	@RequestMapping(value = {"/user-create"}, method = RequestMethod.POST)
-	public String saveUser(@Valid User user, BindingResult result,
-						   ModelMap model) {
-
-		if (result.hasErrors()) {
-			return "registration";
-		}
-
-		/*
-         * Preferred way to achieve uniqueness of field [sso] should be implementing custom @Unique annotation
-		 * and applying it on field [sso] of Model class [User].
-		 *
-		 * Below mentioned peace of code [if block] is to demonstrate that you can fill custom errors outside the validation
-		 * framework as well while still using internationalized messages.
-		 *
-		 */
-		if (!userService.isUserSSOUnique(user.getId(), user.getSsoId())) {
-			FieldError ssoError = new FieldError("user", "ssoId", messageSource.getMessage("non.unique.ssoId", new String[]{user.getSsoId()}, Locale.getDefault()));
-			result.addError(ssoError);
-			return "registration";
-		}
-
-		userService.saveUser(user);
-
-		model.addAttribute("success", "User " + user.getFirstName() + " " + user.getLastName() + " registered successfully");
-		//return "success";
-		return "login";
 	}
 
 
