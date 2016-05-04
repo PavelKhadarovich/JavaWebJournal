60a4138be4a0706ffc1859e8ab61dc23a6de49c0
Dmitry Penda <viento006@gmail.com>
2016-05-05 01:17:34 +0300

    Added autoscroll on state change.

--- src/main/webapp/app/app/app.js
+++ src/main/webapp/app/app/app.js
@@ -1,7 +1,12 @@
-﻿(function(angular) {
+﻿(function (angular) {
     'use strict';
 
     angular.module('javaJournal', [
         'ui.router'
-    ]);
+    ])
+    .run(['$rootScope', function ($rootScope) {
+        $rootScope.$on('$stateChangeSuccess', function () {
+            document.body.scrollTop = document.documentElement.scrollTop = 0;
+        });
+    }]);
 })(angular);
